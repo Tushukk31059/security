eb76a3ecf44de5200b763ac972164bc3d3f95242
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2026-01-03 15:38:11 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {Route,Routes} from "react-router-dom";
+import {Login,Signup} from "./pages";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+   <div>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+     
+    </Routes>
+   </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
+require("dotenv").config();
+const {MONGO_URL,PORT} = process.env;
+app.use(cors({ origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,}))
+app.use(cookieParser());
+app.use(express.json());
+app.use("/",authRoute)
+mongoose.connect(MONGO_URL).then(() => console.log("Mongodb is connected successfully")).catch((err) => console.error(err))
+app.listen(PORT,() => {
+    console.log(`Server is listening on port ${PORT}`)
+});
+
