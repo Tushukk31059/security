7b39179334a5adbb666601ab17a837dc817fac45
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2026-01-08 15:58:13 +0530

    added api url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         navigate("/login");
       }
       const { data } = await axios.post(
-        "http://localhost:3002",
+        "${process.env.API_URL}",
         {},
         { withCredentials: true }
       );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/login",
+        "${process.env.API_URL}/login",
         {
           ...inputValue,
         },
